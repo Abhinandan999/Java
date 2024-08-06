fe38919f0c367fdc2502145433b7165d28092bec
Abhinandan Guruprasad  Madiwal <163436368+Abhinandan999@users.noreply.github.com>
2024-08-06 23:09:42 +0530

    Add files via upload

---
+++ Firstclass.java
@@ -0,0 +1,9 @@
+class Firstclass{
+
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        System.out.print("I love Java \n");
+        System.out.print("I love Coding" );
+    }
+    
+}
